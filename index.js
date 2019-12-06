23c1142976aa84b96d3a1a0c1d80116b4fbf49ca
Tore Hammervoll <hammervoll.tore@gmail.com>
2019-12-06 02:52:18 +0100

    Allow overriding rule
    Useful for exclude/include options

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ function pushRule(config, rule) {
     config.module.rules.push(rule)
 }
 
-module.exports = function rewireSourceMapLoader(config, env) {
+module.exports = function rewireSourceMapLoader(config, env, ruleOverride) {
     getBabelLoader(config, true).options.sourceMaps = true
     getBabelLoader(config, false).options.sourceMaps = true
     pushRule(config, {
         test: /\.(js|mjs|jsx|ts|tsx)$/,
         use: ['source-map-loader'],
-        enforce: 'pre'
+        enforce: 'pre',
+        ...ruleOverride
     })
     return config
 }
